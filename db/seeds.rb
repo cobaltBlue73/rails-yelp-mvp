0f697aad5ac3f21f585143f4fa7f6720dcc302e8
MuhammadZaid Amir <md.zaid73@gmail.com>
2020-09-09 15:25:30 +0800

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Restaurant.destroy_all
+
+(5..10).to_a.sample.times do
+  new_restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  (5..10).to_a.sample.times do
+    Review.new(
+      restaurant: new_restaurant,
+      rating: (0..5).to_a.sample,
+      content: Faker::Restaurant.review
+    ).save!
+  end
+end
